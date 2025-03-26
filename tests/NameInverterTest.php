fe61a1734abe2362181947ed9e833921ce0c2ae7
Jules Pietri <heahdude@yahoo.fr>
2025-03-26 11:04:01 +0100

    red(nameinverter): test empty

---
+++ tests/NameInverterTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class NameInverterTest extends TestCase
+{
+    public function testInvertEmpty(): void
+    {
+        $nameInverter = new NameInverter();
+
+        self::assertSame('', $nameInverter->invert(null));
+    }
+}
