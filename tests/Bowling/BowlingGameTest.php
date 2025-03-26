1ce3fdf5ac2e36439c86965ee918bd120d21ec69
Jules Pietri <heahdude@yahoo.fr>
2025-03-26 13:58:30 +0100

    red(bowling): test all gutters

---
+++ tests/Bowling/BowlingGameTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Bowling;
+
+use PHPUnit\Framework\TestCase;
+
+class BowlingGameTest extends TestCase
+{
+    public function testAllGutters(): void
+    {
+        $game = new BowlingGame();
+
+        for ($i = 0; $i < 20; $i++) {
+            $game->roll(0);
+        }
+
+        self::assertSame(0, $game->getScore());
+    }
+}
