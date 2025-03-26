22881939164f7b04ce3dd403006c51cfbcd0605a
Jules Pietri <heahdude@yahoo.fr>
2025-03-26 15:25:16 +0100

    green(wordwrapper): test short line

---
+++ src/WordWrapper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+class WordWrapper
+{
+    public function wrap(string $s, int $width): string
+    {
+        if (empty($s)) {
+            return '';
+        }
+
+        if (strlen($s) <= $width) {
+            return $s;
+        }
+
+        return $s; // todo cut
+    }
+}
