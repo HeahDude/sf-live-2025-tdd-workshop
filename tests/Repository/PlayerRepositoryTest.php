4fe80ec1ff60df82662d036c02ac9aac7218a279
Jules Pietri <heahdude@yahoo.fr>
2025-03-26 16:39:59 +0100

    red(scroreboard): test one player

--- tests/Repository/PlayerRepositoryTest.php
+++ tests/Repository/PlayerRepositoryTest.php
@@ -2,14 +2,37 @@
 
 namespace App\Tests\Repository;
 
+use App\Entity\GameResult;
+use App\Entity\Player;
 use App\Repository\PlayerRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class PlayerRepositoryTest extends KernelTestCase
 {
     public function testScoreboardEmpty(): void
     {
-        self::assertEmpty($this->getPlayerRepository()->getScoreboard());
+        $scoreboard = $this->getPlayerRepository()->getScoreboard();
+
+        self::assertEmpty($scoreboard);
+    }
+
+    public function testScoreboardWithOneVictory(): void
+    {
+        $player = new Player('player');
+        $result = new GameResult($player, true);
+        $player2 = new Player('player2');
+        $result2 = new GameResult($player2, false);
+
+        /** @var EntityManagerInterface $em */
+        $em = self::getContainer()->get(EntityManagerInterface::class);
+        $em->persist($player);
+        $em->persist($result);
+        $em->persist($player2);
+        $em->persist($result2);
+        $em->flush();
+
+        self::assertCount(1, $this->getPlayerRepository()->getScoreboard());
     }
 
     private function getPlayerRepository(): PlayerRepository
