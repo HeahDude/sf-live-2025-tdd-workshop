3dcf9d1f64c486d2ab0a4714251bd6d9dfec36d6
Jules Pietri <heahdude@yahoo.fr>
2025-03-26 14:36:55 +0100

    green(bowling): test one spare

--- src/Bowling/BowlingGame.php
+++ src/Bowling/BowlingGame.php
@@ -4,15 +4,31 @@ namespace App\Bowling;
 
 class BowlingGame
 {
-    private int $pins = 0;
+    /** @var list<int> */
+    private array $rolls = [];
+    private int $currentRoll = 0;
 
     public function roll(int $pins): void
     {
-        $this->pins += $pins;
+        $this->rolls[$this->currentRoll++] = $pins;
     }
 
     public function getScore(): int
     {
-        return $this->pins;
+        $score = 0;
+        $frameIndex = 0;
+
+        for ($frame = 0; $frame < 10; $frame++) {
+            if (10 === $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1]) {
+                // Spare
+                $score += 10 + $this->rolls[$frameIndex + 2];
+            } else {
+                $score += $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+            }
+
+            $frameIndex += 2;
+        }
+
+        return $score;
     }
 }
